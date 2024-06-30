c29bd4d300537b99e1748b803e2cf0239a5d0689
Wajid Ali <wajid.ali@laksol.com>
2024-06-30 10:44:08 -0700

    update

--- src/main/java/JournalApp/journalApp/config/SecurityConfig.java
+++ src/main/java/JournalApp/journalApp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package JournalApp.journalApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,10 +30,12 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER");
-    }
+//    @Bean
+//    public AuthenticationManager authenticationManager
+//            (AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER");
+//        return auth.getConfigurer();
+//    }
 }
